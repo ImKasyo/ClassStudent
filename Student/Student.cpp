10b29730224670463fbb2cce651fda7b0607b7bc
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2025-09-03 11:21:57 +0300

    Revert "создание открытой части (интерфейса), реализация инициализирующего метода"
    This reverts commit 21e52943b548e19260cc7fd22e4a2caca5f20c27.

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -7,25 +7,6 @@ class Student {
     char* university;
     char* city;
     char* country;
-public:
-    void Init(const char* n, int a, const char* telN, const char* cou, const char* cit, const char* uni) {
-        fullname = new char[strlen(n) + 1];
-        strcpy(fullname, n);
-
-        telefonNumber = new char[strlen(telN) + 1];
-        strcpy(telefonNumber, telN);
-
-        university = new char[strlen(uni) + 1];
-        strcpy(university, uni);
-
-        country = new char[strlen(cou) + 1];
-        strcpy(country, cou);
-
-        city = new char[strlen(cit) + 1];
-        strcpy(city, cit);
-
-        age = a;
-    }
 };
 int main() {
 
