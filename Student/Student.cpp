9503e6f449a5f3484a223ac7d2f46cc99ae01d1a
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2025-09-03 11:01:50 +0300

    создание класса, полей (закрытой части)

---
+++ Student/Student.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Student {
+    char* fullname;
+    int age;
+    char* telefonNumber;
+    char* university;
+    char* city;
+    char* country;
+};
+int main() {
+
+}
